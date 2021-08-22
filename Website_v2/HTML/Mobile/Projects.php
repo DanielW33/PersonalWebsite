975d87c022e8d8cb51a6ed4bdcac705b240ae117
BuildTools <unconfigured@null.spigotmc.org>
2021-08-22 01:14:26 -0400

    Started Implementing mobile version. Contact.php not included due to sensitive information within the php

---
+++ Website_v2/HTML/Mobile/Projects.php
@@ -0,0 +1,40 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name = "description" content = "Daniel Williams, Personal portfolio: Homepage | Mobile Version">
+    <meta name = "viewpoint" content = "width = device-width, initial-scale = 1">
+    <title>Daniel Williams, Personal project portfolio</title>
+    <link href="../../CSS/Mobile/main.css" type="text/css" rel="stylesheet">
+    <link href="../../CSS/Mobile/Navigation.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<div class="MobileNavigation">
+    <a href="main.php">
+        <img src="../../Images/Logos/HomeIcon.png" class="HomeIcon">
+        <h2 class="IconText">Home</h2>
+    </a>
+    <a href="About.php">
+        <img src="../../Images/Logos/person-IconBefore.png" class="AboutIcon">
+        <h2 class="IconText">About</h2>
+    </a>
+    <a href="Skills.php">
+        <img src="../../Images/Logos/GearsIconBefore.png" class="SkillsIcon">
+        <h2 class="IconText">Skills</h2>
+    </a>
+    <a href="Projects.php">
+        <img src="../../Images/Logos/CPUIconBefore.png" class="ProjectsIcon">
+        <h2 class="IconText">Projects</h2>
+    </a>
+    <a href="Contact.php">
+        <img src="../../Images/Logos/EmailBefore.png" class="ContactIcon">
+        <h2 class="IconText">Contact</h2>
+    </a>
+    <div>
+        <img src="../../Images/Logos/MenuClipart.png" class="MenuIcon">
+    </div>
+</div>
+</body>
+</html>
